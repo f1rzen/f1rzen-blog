22ba2878d970d663d78a7de10aa56558918f0895
Ilkay Şafak Baytar <ilkaybaytar@aol.com>
2022-01-27 14:54:09 +0300

    initial commit

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,9 +1,10 @@
 const headerNavLinks = [
+  { href: '/makine-ogrenmesi', title: 'Makine Öğrenmesi' },
+  { href: '/istatistik', title: 'İstatistik' },
   { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
+
   { href: '/projects', title: 'Projects' },
-  { href: '/makine-ogrenmesi', title: 'Makine Öğrenmesi'},
-  { href: '/istatistik', title: 'İstatistik'},
+  { href: '/tags', title: 'Tags' },
   { href: '/about', title: 'About' },
 ]
 
