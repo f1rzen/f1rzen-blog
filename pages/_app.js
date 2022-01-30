65ed6505619f724c6d3b7d4ac1e5bf7ab3d14174
Ilkay Şafak Baytar <ilkaybaytar@aol.com>
2022-01-30 13:04:43 +0300

    Reverting to goatcounter

--- pages/_app.js
+++ pages/_app.js
@@ -3,28 +3,55 @@ import '@/css/prism.css'
 
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
-
+import Script from 'next/script'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
+//import { pageView } from '@/components/analytics/GoatCounter'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(
+    function sendGoatCounterEventsOnRoute() {
+      const handleRouteChange = (path) => {
+        window?.goatcounter?.count?.({
+          path,
+        })
+      }
+      router.events.on('routeChangeComplete', handleRouteChange)
+      return () => {
+        router.events.off('routeChangeComplete', handleRouteChange)
+      }
+    },
+    [router.events]
+  )
+
   return (
-    <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
-      <Head>
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-      </Head>
+    <>
+      <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+        <Head>
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
+        </Head>
 
-      {isDevelopment && isSocket && <ClientReload />}
-      <Analytics />
+        {isDevelopment && isSocket && <ClientReload />}
+        <Analytics />
 
-      <LayoutWrapper>
-        <Component {...pageProps} />
-      </LayoutWrapper>
-    </ThemeProvider>
+        <LayoutWrapper>
+          <Component {...pageProps} />
+        </LayoutWrapper>
+      </ThemeProvider>
+      <Script
+        async
+        data-goatcounter="https://f1rzen-blog.goatcounter.com/count"
+        src="//gc.zgo.at/count.js"
+        strategy="afterInteractive"
+      />
+    </>
   )
 }
