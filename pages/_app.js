195131638d61f5d26fdcae608792f2e087cfbf55
Ilkay Şafak Baytar <ilkaybaytar@aol.com>
2022-01-30 14:31:17 +0300

    SEO fix2

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,6 @@ import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
-//import { pageView } from '@/components/analytics/GoatCounter'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
